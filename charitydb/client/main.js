c1d76a7b46dedd5d082630875dd2b3684c0fa964
nitya <n.narasimhan@gmail.com>
2015-07-29 10:24:44 -0400

    Refactored app

---
+++ charitydb/client/main.js
@@ -0,0 +1,13 @@
+// Lets display data on client
+if (Meteor.isClient) {
+
+  Template.myApp.helpers({
+
+  });
+
+  Template.myApp.events({
+    'click': function () {
+    }
+  });
+}
+
---
+++ charitydb/server/main.js
@@ -0,0 +1,6 @@
+// Let's read in data into Mongo on server side
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
